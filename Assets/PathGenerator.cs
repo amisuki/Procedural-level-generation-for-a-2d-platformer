c4f053c27d0073d1cf31424c8e976b8120fd2573
amisuki <amisuki@gmail.com>
2017-05-06 21:33:49 +0900

    프로젝트용 알고리즘 테스트

--- Assets/PathGenerator.cs
+++ Assets/PathGenerator.cs
@@ -116,7 +116,9 @@ namespace Platformer
 
         Size roomSize;
 
-		public Room[,] m_Rooms;
+        public List<Pointer> pathLoad = new List<Pointer>();
+
+        public Room[,] m_Rooms;
         public Room[,] rooms { get { return m_Rooms; } }
 
 
@@ -126,7 +128,8 @@ namespace Platformer
 		Pointer m_Exit;
         public Pointer exit { get { return m_Exit; } }
 
-        KindDice pathDice  = new KindDice(1, 6);
+        KindDice pathDice  = new KindDice(1, 8);
+
 
 		[System.Serializable]
 		public class Room 
@@ -161,7 +164,9 @@ namespace Platformer
                 Debug.Log("error index : " + index.x + " : " + index.y);
                 Debug.DebugBreak();
             }
-            
+
+            pathLoad.Add(new Pointer(index.x, index.y));
+            Debug.Log(string.Format("x = {0}, y= {1}", index.x, index.y));
             m_Rooms [index.x, index.y].type = (int)type;
 		}
 
@@ -181,7 +186,7 @@ namespace Platformer
 		{
 			Pointer index	= new Pointer();
 			index.x = UnityEngine.Random.Range (0, (int)m_MapSize.x);
-			index.y = 0;
+			index.y = UnityEngine.Random.Range(0, (int)m_MapSize.y);
 			m_Entrance = index;
 		}
 
@@ -190,76 +195,116 @@ namespace Platformer
 			Pointer iterator = m_Entrance;
             SetRoomType(iterator, eRoomType.LeftRight );
 
-			int dice = pathDice.GetRandom ();
+            pathLoad.Clear();
+
+            int dice = pathDice.GetRandom ();
 			iterator = CalcuDiceRull(iterator, dice);
-            int deadCount = 50;
+            int deadCount = 120;
+            int Counting = 15;
             while (iterator != m_Exit && deadCount >= 0)
             {
                 dice = pathDice.GetRandom();
+                Pointer temp = iterator;
                 iterator = CalcuDiceRull(iterator, dice);
                 //yield return new WaitForSeconds(0.3f);
                 --deadCount;
+                if( deadCount <= 0)
+                    m_Exit = iterator;
+
+                if (temp != iterator)
+                    Counting--;
+
+
+                if (Counting <= 0)
+                    m_Exit = iterator;
+            }
+
+            int distnace = Mathf.Abs((entrance.x + entrance.y) - (m_Exit.x + m_Exit.y));
+            if (distnace < 4)
+            {
+                m_Rooms = new Room[m_MapSize.x, m_MapSize.y];
+                for (int x = 0; x < m_MapSize.x; ++x)
+                {
+                    for (int y = 0; y < m_MapSize.y; ++y)
+                    {
+                        m_Rooms[x, y] = new Room();
+                    }
+                }
+                GeneratorPath();
             }
 
             //yield return null;
 		}
 
-        bool LeftNotSolid(int dice, Pointer temp)
+        bool isValid(Pointer it)
+        {
+            if(it.x < 0 && it.x < mapSize.x)
+            {
+                if (it.y < 0 && it.y < mapSize.y)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        bool LeftSolid(int dice, Pointer temp)
         {
             if (DiceRull.isLeft(dice))
             {
                 temp.x--;
                 if (temp.x <= 0)
                 {
-                    temp.x = 0;
+                    return false;
                 }
-                if (GetRoomType(temp) != 0)
+                if (GetRoomType(temp) == 0)
                     return true;
             }
             return false;
         }
 
 
-        bool RightNotSolid(int dice, Pointer temp)
+        bool RightSolid(int dice, Pointer temp)
         {
             if (DiceRull.isRight(dice))
             {
                 temp.x++;
-                if (temp.x >= m_MapSize.x - 1)
+                if (temp.x >= m_MapSize.x)
                 {
-                    temp.x = m_MapSize.x - 1;
+                    return false;
                 }
-                if (GetRoomType(temp) != 0)
+                if (GetRoomType(temp) == 0)
                     return true;
             }
             return false;
         }
 
-        bool TopNotSolid(int dice, Pointer temp)
+        bool TopSolid(int dice, Pointer temp)
         {
             if (DiceRull.isTop(dice))
             {
-                temp.x--;
-                if (temp.x <= 0)
+                temp.y++;
+                if (temp.y >= m_MapSize.y)
                 {
-                    temp.x = 0;
+                    return false;
                 }
-                if (GetRoomType(temp) != 0)
+                if (GetRoomType(temp) == 0)
                     return true;
             }
             return false;
         }
 
-        bool DownNotSolid(int dice, Pointer temp)
+        bool DownSolid(int dice, Pointer temp)
         {
             if (DiceRull.isDown(dice))
             {
-                temp.x--;
-                if (temp.x <= 0)
+                temp.y--;
+                if (temp.y <= 0)
                 {
-                    temp.x = 0;
+                    return false;
                 }
-                if (GetRoomType(temp) != 0)
+                if (GetRoomType(temp) == 0)
                     return true;
             }
             return false;
@@ -267,71 +312,47 @@ namespace Platformer
 
         Pointer CalcuDiceRull(Pointer index, int dice)
 		{
-            if (LeftNotSolid(dice, index) || RightNotSolid(dice, index))
+            if (DiceRull.isDown(dice) && DownSolid(dice, index) == false)
+                return index;
+            if (DiceRull.isTop(dice) && TopSolid(dice, index) == false)
+                return index;
+            if (DiceRull.isRight(dice) && RightSolid(dice, index) == false)
+                return index;
+            if (DiceRull.isLeft(dice) && LeftSolid(dice, index) == false)
                 return index;
 
+            //if (LeftNotSolid(dice, index) || RightNotSolid(dice, index)
+            //    || DownNotSolid(dice, index) || TopNotSolid(dice, index))
+            //    return index;
 
             Pointer nextRoom = index;
+
             if (DiceRull.isDown(dice))
             {
-                if(GetRoomType(nextRoom) != 3)
-                    SetRoomType(nextRoom, eRoomType.Down);
+                SetRoomType(nextRoom, eRoomType.Down);
+                nextRoom.y--;
+                SetRoomType(nextRoom, eRoomType.Top);
 
+                return nextRoom;
+            }
+
+            else if (DiceRull.isTop(dice))
+            {
+                SetRoomType(nextRoom, eRoomType.Top);
                 nextRoom.y++;
-                if(nextRoom.y >= m_MapSize.y)
-                {
-                    nextRoom.y = m_MapSize.y - 1;
-                    m_Exit = nextRoom;
-					SetRoomType(nextRoom, eRoomType.Top);
-                }
-                else
-                    SetRoomType(nextRoom, eRoomType.Top);
+                SetRoomType(nextRoom, eRoomType.Down);
 
                 return nextRoom;
             }
             else if (DiceRull.isLeft(dice))
             {
                 nextRoom.x--;
-                if (nextRoom.x <= 0)
-                {
-                    nextRoom.x = 0;
-                    SetRoomType(nextRoom, eRoomType.Down);
-                    nextRoom.y++;
-                    if (nextRoom.y >= m_MapSize.y)
-                    {
-                        nextRoom.y = m_MapSize.y - 1;
-                        m_Exit = nextRoom;
-                        SetRoomType(nextRoom, eRoomType.LeftRight);
-                    }
-                    else
-                        SetRoomType(nextRoom, eRoomType.Top);
-                }
-                else
-                {
-                    SetRoomType(nextRoom, eRoomType.LeftRight);
-                }
+                SetRoomType(nextRoom, eRoomType.LeftRight);
             }
             else if (DiceRull.isRight(dice))
             {
                 nextRoom.x++;
-                if (nextRoom.x >= m_MapSize.x - 1)
-                {
-                    nextRoom.x = m_MapSize.x - 1;
-                    SetRoomType(nextRoom, eRoomType.Down);
-                    nextRoom.y++;
-                    if (nextRoom.y >= m_MapSize.y)
-                    {
-                        nextRoom.y = m_MapSize.y - 1;
-                        m_Exit = nextRoom;
-                        SetRoomType(nextRoom, eRoomType.LeftRight);
-                    }
-                    else
-                        SetRoomType(nextRoom, eRoomType.Top);
-                }
-                else
-                {
-                    SetRoomType(nextRoom, eRoomType.LeftRight);
-                }
+                SetRoomType(nextRoom, eRoomType.LeftRight);
             }
 
             return nextRoom;
@@ -356,6 +377,7 @@ namespace Platformer
         }
 
         public Text text;
+        public Text DebugText;
 
         void Awake () 
 		{
@@ -372,6 +394,8 @@ namespace Platformer
 
             //StartCoroutine (mapData.GeneratorPath ());
             m_MapData.GeneratorPath();
+
+            StartCoroutine(SequenceGUI());
         }
 
         public void DrawGUI()
@@ -386,13 +410,47 @@ namespace Platformer
                 string line = "";
                 for (int x = 0; x < MapSizeX; ++x)
                 {
-                    line += m_MapData.m_Rooms[x, y].type.ToString();
+                    if(m_MapData.entrance.x == x && m_MapData.entrance.y == y)
+                    {
+                        line += "S";
+                    }
+                    else if (m_MapData.exit.x == x && m_MapData.exit.y == y)
+                    {
+                        line += "E";
+                    }
+                    else
+                        line += m_MapData.m_Rooms[x, y].type.ToString();
                 }
                 text.text += line + "\n";
             }
 
             text.text += "\nexit = x : " + m_MapData.exit.x + "  y : " + m_MapData.exit.y + "\n";
         }
-	}
+
+        IEnumerator SequenceGUI()
+        {
+            yield return new WaitForSeconds(1);
+            for (int i = 0; i < m_MapData.pathLoad.Count; ++i)
+            {
+                //DebugGUI(m_MapData.pathLoad[i].x, m_MapData.pathLoad[i].y);
+                yield return new WaitForSeconds(0.3f);
+            }
+            
+        }
+
+        void DebugGUI(int xp, int yp)
+        {
+            string line = DebugText.text;
+            for (int y = 0; y < MapSizeY; ++y)
+            {
+                for (int x = 0; x < MapSizeX; ++x)
+                {
+                    //if (y == yp && x == xp)
+                        //line.Rep = m_MapData.m_Rooms[x, y].type.ToString()[0];
+                }
+                DebugText.text += line + "\n";
+            }
+        }
+    }
 
 }
